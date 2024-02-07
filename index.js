214250bc9ea9563a5660ba4cef0dcfe6bc0da6c4
José Pedro Dias <jose.dias@arkadium.com>
2024-02-07 13:19:02 +0000

    reviewed last srt from yesterday

---
+++ index.js
@@ -0,0 +1,108 @@
+const timeEl = document.getElementById('time');
+const durationEl = document.getElementById('duration');
+const togglePlayEl = document.getElementById('toggle-play');
+const rew15El = document.getElementById('rew-15');
+const ffw15El = document.getElementById('ffw-15');
+const progressEl = document.getElementById('progress');
+const contentEl = document.getElementById('content');
+
+let subEls, audio, subtitles, currentSubIndex;
+currentSubIndex = -1;
+
+const highlightSub = (idx) => {
+    subEls.forEach((subEl, i) => subEl.classList.toggle('highlight', i === idx));
+    subEls[idx]?.scrollIntoView({ behavior: 'auto', block: 'center', inline: 'center' });
+}
+
+const onClickContentEl = (el) => {
+    const idx = subEls.indexOf(el);
+    const sub = subtitles[idx];
+    if (!sub) return;
+    currentSubIndex = idx;
+    audio.currentTime = sub.start;
+    highlightSub(currentSubIndex);
+    if (audio.paused) audio.play();
+    togglePlayEl.focus();
+}
+
+contentEl.addEventListener('click', (ev) => onClickContentEl(ev.target));
+
+async function run(name) {
+    if (audio) audio.pause();
+
+    const p = (await player('content/' + name));
+    ({ audio, subtitles } = p);
+
+    contentEl.innerHTML = '';
+    subEls = [];
+
+    for (const { content } of subtitles) {
+        const divEl = document.createElement('div');
+        divEl.appendChild(document.createTextNode(content));
+        contentEl.appendChild(divEl);
+        subEls.push(divEl);
+    }
+
+    const onDuration = () => {
+        const d = audio.duration;
+        durationEl.innerHTML = humanTime(d);
+        progressEl.max = Math.ceil(d);
+    }
+
+    const move = (deltaSecs) => {
+        audio.currentTime = audio.currentTime + deltaSecs;
+        if (audio.paused) audio.play();
+        togglePlayEl.focus();
+    }
+
+    if (audio.duration) onDuration();
+    audio.addEventListener('loadedmetadata', onDuration);
+    audio.addEventListener('timeupdate', () => {
+        const t = audio.currentTime;
+        const ti = Math.round(t);
+        const ts = humanTime(ti);
+        if (timeEl.innerHTML != ts) timeEl.innerHTML = ts;
+        if (ti != progressEl.value) progressEl.value = ti;
+
+        let sub = subtitles[currentSubIndex];
+        if (!sub || t < sub.start || t > sub.end) {
+            currentSubIndex = subtitles.findIndex((s, i) => s && t >= s.start && t <= s.end);
+            highlightSub(currentSubIndex);
+        }
+    });
+
+    progressEl.addEventListener('change', () => {
+        audio.currentTime = progressEl.value;
+        togglePlayEl.focus();
+    });
+
+    togglePlayEl.addEventListener('click', () => {
+        audio.paused ? audio.play() : audio.pause();
+    });
+
+    togglePlayEl.addEventListener('keydown', (ev) => {
+        let deltaSecs = 0;
+        let deltaIndex = 0;
+        if      (ev.key === 'ArrowLeft')  deltaSecs = -15;
+        else if (ev.key === 'ArrowRight') deltaSecs =  15;
+        else if (ev.key === 'ArrowUp')    deltaIndex = -1;
+        else if (ev.key === 'ArrowDown')  deltaIndex =  1;
+
+        if (deltaSecs || deltaIndex) {
+            ev.preventDefault();
+            ev.stopPropagation();
+            if (deltaSecs) {
+                move(deltaSecs);
+            } else {
+                const el = subEls[currentSubIndex + deltaIndex];
+                if (el) onClickContentEl(el);
+            }
+        }
+    });
+
+    rew15El.addEventListener('click', () => move(-15));
+    ffw15El.addEventListener('click', () => move( 15));
+
+    audio.play();
+    togglePlayEl.focus();
+}
